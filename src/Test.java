0758531564d323ef716a994120a3b602957e624c
gieks <brave.unicorn573@gmail.com>
2013-07-10 20:38:23 +0900

    tes

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+
+/*
+ * name:
+ * ahthorF kiha
+ */
+
+public class Test {
+
+	// ‹N“®ˆ—
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.main();
+	}
+
+	// ƒƒCƒ“
+	void main() {
+
+	}
+
+}
